a6c8c7b697c17d883404fc4e578cc6505575bdf8
rene <prene@tlu.ee>
2025-02-18 21:39:40 +0200

    Delete 02_14.02_veateated/decathlon directory
    spring failid jäid maha

--- 02_14.02_veateated/decathlon/repository/AthleteRepository.java
+++
@@ -1,7 +0,0 @@
-package ee.rene.decathlon.repository;
-
-import ee.rene.decathlon.entity.Athlete;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AthleteRepository extends JpaRepository<Athlete, Long> {
-}
